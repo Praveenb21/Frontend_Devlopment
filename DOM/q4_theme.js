171aa15de2e7e78ead6c7dc4b454831a7b16fa49
Praveen Bhagour <168171538+Praveenb21@users.noreply.github.com>
2025-11-25 11:04:55 +0530

    Remove DOM and JS_ES directories from Jquery branch

--- DOM/q4_theme.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-document.addEventListener("click", (e) => {
-  if (e.target.tagName === "BUTTON") {
-    const theme = e.target.getAttribute("data-set");
-    document.body.setAttribute("data-theme", theme);
-  }
-});
